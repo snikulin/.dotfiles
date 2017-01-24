82b9942adcc5a6384b112cee915733ffbdda7e3e
Sergey Nikulin <snikulin@gmail.com>
2017-01-25 02:50:50 +0300

    remap cas-lock and esc keys

--- mklinks.sh
+++ mklinks.sh
@@ -3,4 +3,4 @@ ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
+ln -sf ~/.dotfiles/.speedswapper ~/.speedswapper
