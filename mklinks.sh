39e1ef65e34ebf87a15d234429de2cb9b8432e84
Sergey Nikulin <snikulin@gmail.com>
2017-01-26 16:01:15 +0300

    make link for .xinitrc

--- mklinks.sh
+++ mklinks.sh
@@ -4,3 +4,4 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.speedswapper ~/.speedswapper
+ln -sf ~/.dotfiles/.xinitrc ~/.xinitrc
