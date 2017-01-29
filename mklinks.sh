ee82c559df010028f84b9db832f89ca06ed370b8
Sergey Nikulin <snikulin@gmail.com>
2017-01-29 03:05:31 +0300

    let's try neovim ;)

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
