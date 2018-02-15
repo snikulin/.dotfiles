32c8307fc8f441191b602baadac09659a5a5e918
Sergey Nikulin <snikulin@gmail.com>
2018-02-16 00:19:53 +0300

    i3 config

--- mklinks.sh
+++ mklinks.sh
@@ -7,6 +7,7 @@ ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.psqlrc ~/.psqlrc
+ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
 
 if [[ `uname` = "Linux" ]]; then
   dconf write /org/gnome/desktop/input-sources/xkb-options "['grp_led:scroll']"
