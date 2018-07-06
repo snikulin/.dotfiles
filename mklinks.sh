e3a4f114a08e88696f5fef81d26e6db3b8cf8653
Sergey Nikulin <snikulin@gmail.com>
2018-07-06 13:58:26 +0300

    use i3blocks instead of polybar

--- mklinks.sh
+++ mklinks.sh
@@ -2,13 +2,15 @@
 ln -sf ~/.dotfiles/.bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.config
+ln -sf ~/.dotfiles/.scripts -t ~/
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.psqlrc ~/.psqlrc
-ln -sf ~/.dotfiles/.config/i3 ~/.config/i3
-ln -sf ~/.dotfiles/.config/polybar ~/.config/polybar
+ln -sf ~/.dotfiles/.config/i3 -t ~/.config
+ln -sf ~/.dotfiles/.config/i3blocks -t ~/.config
+ln -sf ~/.dotfiles/.config/polybar -t ~/.config
 
 if [[ `uname` = "Linux" ]]; then
   dconf write /org/gnome/desktop/input-sources/xkb-options "['grp_led:scroll']"
