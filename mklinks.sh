ab8153f0b9ad221f8a4c73d2e0a9db520cb4ac8e
Sergey Nikulin <snikulin@gmail.com>
2023-01-02 01:19:54 +0300

    better symlinks handling

--- mklinks.sh
+++ mklinks.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-stow zsh
-stow tmux
-stow nvim
-stow scripts
-stow fortune
-stow alacritty
+stow --no-folding zsh
+stow --no-folding tmux
+stow --no-folding nvim
+stow --no-folding scripts
+stow --no-folding fortune
+stow --no-folding alacritty
 
 if [[ `uname` = "Darwin" ]]; then
-  stow macos
+  stow --no-folding macos
 fi
 if [[ `uname` = "Linux" ]]; then
-  stow linux
-  dconf write /org/gnome/desktop/input-sources/xkb-options "['grp_led:scroll']"
+  stow --no-folding linux
 fi
